8c66b2fb620e1be14c70694efce04851ddb167c4
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-06-27 19:11:10 +0100

    bartender coming along nicely

--- lib/pub/bartender.rb
+++ lib/pub/bartender.rb
@@ -1,9 +1,33 @@
 class Pub
   class Bartender
-    attr_reader :pub_name
-
     def initialize(pub_name)
       @pub_name = pub_name
+    end
+
+    # Serves an order.
+    def serve(drink, &block)
+      Pub.counter.publish([@pub_name, order].join(':'), block.call)
+    end
+
+    # Takes one or more orders.
+    #
+    #   drinks = bartender.take_orders(3)
+    #   drinks.each do |drink|
+    #       bartender.serve(drink) do
+    #       # prepare drink
+    #     end
+    #   end
+    def take_orders(count = 1)
+      drinks = Array.new
+
+      # Seems I can't use a simple map here.
+      count.times do
+        drink = Pub.counter.lpop(@pub_name)
+        break if drink.nil?
+        drinks << drink
+      end
+
+      drinks
     end
   end
 end
--- spec/pub/bartender_spec.rb
+++ spec/pub/bartender_spec.rb
@@ -2,5 +2,23 @@ require "spec_helper"
 
 class Pub
   describe Bartender do
+    include PubStubbers
+
+    describe "#take_order" do
+      it "takes one or more orders to fill" do
+        enter_pub do |pub|
+          bartender = pub.new_bartender
+          stub_orders(pub, 'Guinness', 'Stella', '1664')
+          bartender.take_orders(2).should =~ ['Guinness', 'Stella']
+          bartender.take_orders(2).should eql ['1664']
+        end
+      end
+    end
+
+    describe "#serve" do
+      it "serves a drink" do
+        pending
+      end
+    end
   end
 end
