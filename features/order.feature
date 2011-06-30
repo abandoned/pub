62f060e56cc925e5adb2772e155859f6c935ec3a
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-06-30 14:06:03 +0100

    make cuke steps awesome

--- features/order.feature
+++ features/order.feature
@@ -1,42 +1,63 @@
-Feature: Order drinks
+Feature: Order beers
   As a pub patron  
   I want bartenders to take orders asynchronously  
   So I do not block the counter  
 
   Below we assume a bartender can fill an order in one second.
 
-  Scenario: Order a drink
+  Scenario: Order a beer
     Given 1 bartender
-    When I order:
-      | drink          |
+    When "John" orders:
+      | beer           |
       | Brooklyn Lager |
-    Then I should receive my drink in 1 second
+    Then "John" should receive his beer in 1 second
 
-  Scenario: Order multiple drinks
+  Scenario: Order multiple beers
     Given 1 bartender
-    When I order:
-      | drink          |
+    When "John" orders:
+      | beer           |
       | Brooklyn Lager |
       | Efes Pilsen    |
-    Then I should receive my drinks in 2 seconds
+    Then "John" should receive his beers in 2 seconds
 
   Scenario: Two bartenders
     Given 2 bartenders
-    When I order:
-      | drink          |
+    When "John" orders:
+      | beer           |
       | Brooklyn Lager |
       | Efes Pilsen    |
-    Then I should receive my drinks in 1 second
+    Then "John" should receive his beers in 1 second
 
   Scenario: Timeout
     Given 1 bartender
-    And I have no patience to wait more than 2 seconds at the counter
-    When I order:
-      | drink          |
+    And "John" has no patience to wait more than 2 seconds at the counter
+    When "John" orders:
+      | beer           |
       | Brooklyn Lager |
       | Efes Pilsen    |
       | Stella         |
-    Then I should receive the following drinks in 2 seconds:
-      | drink                    |
+    Then "John" should receive the following beers in 2 seconds:
+      | beer                    |
       | A pint of Brooklyn Lager |
       | A pint of Efes Pilsen    |
+
+  Scenario: Two patrons order the same beer
+    Given 1 bartender
+    And "John" orders:
+      | beer           |
+      | Brooklyn Lager |
+    And "Jane" orders:
+      | beer           |
+      | Brooklyn Lager |
+    Then "John" should receive his beer in 1 second
+    And "Jane" should receive her beer in 1 second
+
+  Scenario: Crowd
+    Given 10 bartenders
+    When 10 patrons order 1 beer each
+    Then they should receive their beers within 1 second
+
+  Scenario: Overcrowding
+    Given 2 bartenders
+    When 4 patrons order 2 beers each
+    Then they should receive their beers within 4 seconds
--- features/step_definitions/pub_steps.rb
+++ features/step_definitions/pub_steps.rb
@@ -1,22 +1,39 @@
 module PubHelpers
-  def create_patron(name)
-    instance_variable_set("@#{name}", @pub.new_patron)
+  def data_store(name)
+    patron_names << name unless patron_names.include?(name)
+
+    instance_variable_get("@hash_of_#{name}") ||
+    instance_variable_set("@hash_of_#{name}", Hash.new)
   end
 
-  def find_patron(name)
-    instance_variable_get("@#{name}")
+  def find_or_create_patron(name)
+    instance_variable_get("@#{name}") ||
+    instance_variable_set("@#{name}", pub.new_patron)
+  end
+
+  def map_beers(table)
+    table.hashes.map { |hash| hash["beer"] }
+  end
+
+  def now
+    Time.now
   end
 
   def orders
     @orders ||= Hash.new
   end
 
-  def patron
-    @patron ||= pub.new_patron
+  def patron_names
+    @patron_names ||= []
   end
 
   def pub
     @pub ||= Pub.new("Ye Olde Rubies")
+  end
+
+  def sleep_until_order_complete(name)
+    fiber = data_store(name)[:fiber]
+    EM::Synchrony.sleep(0.1) while fiber.alive?
   end
 end
 
@@ -35,23 +52,61 @@ Given /^(\d+) bartenders?$/ do |count|
   end
 end
 
-Given /^I have no patience to wait more than (\d+) seconds at the counter$/ do |seconds|
+Given /^"([^"]+)" has no patience to wait more than (\d+) seconds at the counter$/ do |name, seconds|
+  patron = find_or_create_patron(name)
   patron.instance_variable_set(:@timeout, seconds)
 end
 
-When /^I order:$/ do |table|
-  drinks = table.hashes.map { |hash| hash["drink"] }
+When /^"([^"]+)" orders:$/ do |name, table|
+  beers  = map_beers(table)
+  patron = find_or_create_patron(name)
 
-  @started_at = Time.now
-  @tray       = patron.order(*drinks)
+  data_store(name)[:started_at] = Time.now
+
+  fiber = Fiber.new do
+    tray = patron.order(*beers)
+    data_store(name)[:tray] = tray
+  end
+  data_store(name)[:fiber] = fiber
+
+  EM.next_tick { fiber.resume }
 end
 
-Then /^I should receive my drinks? in (\d+) seconds?$/ do |seconds|
-  (Time.now - @started_at).should be_within(0.1).of(seconds)
+When /^(\d+) patrons order (\d+) beers? each$/ do |patrons_count, beers_count|
+  patrons_count.times do |patron_counter|
+    raw = beers_count.times.inject("| beer |\n") do |raw, beer_counter|
+      raw << "| beer_#{patron_counter}_#{beer_counter} |\n"
+    end
+    name = "patron_#{patron_counter}"
+    When %{"#{name}" orders:}, table(raw)
+  end
 end
 
-Then /^I should receive the following drinks? in (\d+) seconds?:$/ do |seconds, table|
-  (Time.now - @started_at).should be_within(0.1).of(seconds)
-  drinks = table.hashes.map { |hash| hash["drink"] }
-  @tray.should =~ drinks
+Then /^"([^"]+)" should receive (?:his|her) beers? in (\d+) seconds?$/ do |name, seconds|
+  sleep_until_order_complete(name)
+
+  started_at = data_store(name)[:started_at]
+  (now - started_at).should be_within(0.2).of(seconds)
+end
+
+Then /^"([^"]+)" should receive the following beers? in (\d+) seconds?:$/ do |name, seconds, table|
+  sleep_until_order_complete(name)
+
+  started_at = data_store(name)[:started_at]
+  (now - started_at).should be_within(0.2).of(seconds)
+
+  beers = map_beers(table)
+  tray = data_store(name)[:tray]
+  tray.should =~ beers
+end
+
+Then /^they should receive their beers within (\d+) seconds?$/ do |seconds|
+  started_at  = now
+  patron_names.each do |name|
+    patron_started_at = data_store(name)[:started_at]
+    started_at = patron_started_at if patron_started_at < started_at
+    sleep_until_order_complete(name)
+  end
+
+  (now - started_at).should be_within(0.2).of(seconds)
 end
