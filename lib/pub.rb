ca4d25a3cabd6cc63c56d6cfb50b2c4c18f62997
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-07-12 17:24:36 +0100

    edit source commentary

--- lib/pub.rb
+++ lib/pub.rb
@@ -7,15 +7,7 @@ require "pub/bartender"
 require "pub/house"
 require "pub/patron"
 
-# A Redis-backed pub with a non-blocking bar counter.
-#
-# Or, putting aside the metaphor for a moment:
-#
-# A processing queue where consumers, instead of simply queuing jobs and
-# getting on with their lives, queue and wait for a response without
-# blocking the Ruby process.
-#
-# Each public house is a distinct queue.
+# A pub with a non-blocking bar counter.
 module Pub
   extend self
 
